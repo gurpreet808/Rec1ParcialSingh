5a9ac02d1d661f1da0aa50cf28ab96b63f554c41
gurpreet <gurpreets808@gmail.com>
2023-12-07 12:38:30 -0300

    Agrego pages home y error404

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', loadChildren: () => import('./pages/home/home.module').then(m => m.HomeModule) },
+  { path: '**', loadChildren: () => import('./pages/error404/error404.module').then(m => m.Error404Module) }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],

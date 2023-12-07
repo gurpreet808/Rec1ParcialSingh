3b39d7c5b6dd02fc078a1791056b55b11cd851b5
gurpreet <gurpreets808@gmail.com>
2023-12-07 17:54:49 -0300

    Creo entidades

---
+++ src/app/clases/helado.ts
@@ -0,0 +1,7 @@
+export interface Helado {
+    id: string;
+    nombre: string;
+    tipo: string;
+    precio: number;
+    pesoAproximado: number;
+}
---
+++ src/app/clases/repartidor.ts
@@ -0,0 +1,7 @@
+export interface Repartidor {
+    id: string;
+    dni: string;
+    capacidadTransporte: number;
+    paisOrigen: string;
+    unidadPropia: boolean;
+}

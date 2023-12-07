59a7d4ba805bfd57d1d3f9d649158b3d45e8eaac
gurpreet <gurpreets808@gmail.com>
2023-12-07 12:22:21 -0300

    Agrego archivos iniciales

---
+++ src/app/app.module.ts
@@ -0,0 +1,42 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import { getAnalytics, provideAnalytics, ScreenTrackingService, UserTrackingService } from '@angular/fire/analytics';
+import { initializeAppCheck, ReCaptchaEnterpriseProvider, provideAppCheck } from '@angular/fire/app-check';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+import { getDatabase, provideDatabase } from '@angular/fire/database';
+import { getFunctions, provideFunctions } from '@angular/fire/functions';
+import { getMessaging, provideMessaging } from '@angular/fire/messaging';
+import { getPerformance, providePerformance } from '@angular/fire/performance';
+import { getStorage, provideStorage } from '@angular/fire/storage';
+import { getRemoteConfig, provideRemoteConfig } from '@angular/fire/remote-config';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    provideFirebaseApp(() => initializeApp({ "projectId": "singh-primerparciallab4", "appId": "1:501824118122:web:67d93d342b1d93d5f95913", "storageBucket": "singh-primerparciallab4.appspot.com", "apiKey": "AIzaSyBJdqqZZHbc5I7NjyHcLXx74urVQnjDDpk", "authDomain": "singh-primerparciallab4.firebaseapp.com", "messagingSenderId": "501824118122", "measurementId": "G-GF8LN7C5DJ" })),
+    provideAuth(() => getAuth()),
+    provideAnalytics(() => getAnalytics()),
+    provideFirestore(() => getFirestore()),
+    provideDatabase(() => getDatabase()),
+    provideFunctions(() => getFunctions()),
+    provideMessaging(() => getMessaging()),
+    providePerformance(() => getPerformance()),
+    provideStorage(() => getStorage()),
+    provideRemoteConfig(() => getRemoteConfig())
+  ],
+  providers: [
+    ScreenTrackingService,
+    UserTrackingService
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }

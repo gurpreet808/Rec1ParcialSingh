702e1998450abcb56e84b3dc9db1a912d3deb597
gurpreet <gurpreets808@gmail.com>
2023-12-07 17:53:40 -0300

    Agrego PrimeNG PrimeFlex y FontAwesome

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
-import { HttpClientModule } from '@angular/common/http';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
 import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
+import { ConfirmationService, MessageService } from 'primeng/api';
+
 import { getAuth, provideAuth } from '@angular/fire/auth';
-import { getAnalytics, provideAnalytics, ScreenTrackingService, UserTrackingService } from '@angular/fire/analytics';
 import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 import { getDatabase, provideDatabase } from '@angular/fire/database';
 import { getFunctions, provideFunctions } from '@angular/fire/functions';
@@ -17,6 +19,8 @@ import { getStorage, provideStorage } from '@angular/fire/storage';
 import { getRemoteConfig, provideRemoteConfig } from '@angular/fire/remote-config';
 
 import { NavBarComponent } from './componentes/nav-bar/nav-bar.component';
+import { HttpClientModule } from '@angular/common/http';
+import { PrimeNGModule } from './modulos/prime-ng/prime-ng.module';
 
 @NgModule({
   declarations: [
@@ -26,21 +30,36 @@ import { NavBarComponent } from './componentes/nav-bar/nav-bar.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
+    BrowserAnimationsModule,
     HttpClientModule,
-    provideFirebaseApp(() => initializeApp({ "projectId": "singh-primerparciallab4", "appId": "1:501824118122:web:67d93d342b1d93d5f95913", "storageBucket": "singh-primerparciallab4.appspot.com", "apiKey": "AIzaSyBJdqqZZHbc5I7NjyHcLXx74urVQnjDDpk", "authDomain": "singh-primerparciallab4.firebaseapp.com", "messagingSenderId": "501824118122", "measurementId": "G-GF8LN7C5DJ" })),
+    provideFirebaseApp(
+      () => initializeApp(
+        {
+          "projectId": "singh-primerparciallab4",
+          "appId": "1:501824118122:web:67d93d342b1d93d5f95913",
+          "storageBucket": "singh-primerparciallab4.appspot.com",
+          "apiKey": "AIzaSyBJdqqZZHbc5I7NjyHcLXx74urVQnjDDpk",
+          "authDomain": "singh-primerparciallab4.firebaseapp.com",
+          "messagingSenderId": "501824118122",
+          "measurementId": "G-GF8LN7C5DJ"
+        }
+      )
+    ),
     provideAuth(() => getAuth()),
-    provideAnalytics(() => getAnalytics()),
     provideFirestore(() => getFirestore()),
     provideDatabase(() => getDatabase()),
     provideFunctions(() => getFunctions()),
     provideMessaging(() => getMessaging()),
     providePerformance(() => getPerformance()),
     provideStorage(() => getStorage()),
-    provideRemoteConfig(() => getRemoteConfig())
+    provideRemoteConfig(() => getRemoteConfig()),
+    PrimeNGModule
   ],
   providers: [
-    ScreenTrackingService,
-    UserTrackingService
+    MessageService,
+    ConfirmationService
   ],
   bootstrap: [AppComponent]
 })
